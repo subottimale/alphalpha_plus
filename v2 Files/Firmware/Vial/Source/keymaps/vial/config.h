ec1ee1b4209ec5f666c104e971c506dc1676b692
OTTIMO <69826495+subottimale@users.noreply.github.com>
2024-03-21 19:38:24 -0700

    add vial keymap files

---
+++ v2 Files/Firmware/Vial/Source/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAF, 0x83, 0x6F, 0xEF, 0xF7, 0x7E, 0xCF, 0x13}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 9}
